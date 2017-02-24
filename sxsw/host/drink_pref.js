eb1ac82e349099df16fd38d36741bbb64fea83d3
Thomas Rushton <trushton134@gmail.com>
2017-02-23 16:51:02 -0800

    trying to get back

--- sxsw/host/drink_pref.js
+++
@@ -1,9 +0,0 @@
-function finishRegistration(value){
-    console.log(localStorage.getItem('user_id'));
-    firebase.database().ref('users/'+ localStorage.getItem('user_id')).update({
-        drink_pref: value.toString()
-    }).then(function(){
-        window.location = "/sxsw/host/completedRegistration.html";
-    });
-
-}
