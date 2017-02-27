c5e9a6b66c0f78f8e5399e44cfcf8a1808fdfa1a
Thomas Rushton <trushton134@gmail.com>
2017-02-27 14:12:20 -0800

    fix frinds

--- sxsw/host/drink_pref.js
+++ sxsw/host/drink_pref.js
@@ -1,6 +1,6 @@
 function finishRegistration(value){
-    console.log(localStorage.getItem('user_id'));
-    firebase.database().ref('users/'+ localStorage.getItem('user_id')).update({
+    console.log(localStorage.getItem('currentDevice'));
+    firebase.database().ref('users/'+ localStorage.getItem('currentDevice')).update({
         drink_pref: value.toString()
     }).then(function(){
         window.location = "/sxsw/host/completedRegistration.html";
