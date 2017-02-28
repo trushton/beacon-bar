527ff57df2038a188a72422650487bde02ab337c
Thomas Rushton <trushton134@gmail.com>
2017-02-28 07:39:14 -0800

    Adding bar kiosk

---
+++ barKiosk/host/immediate.js
@@ -0,0 +1,74 @@
+var drinks = {
+    "Vodka": {
+        "Walmart Blue": {
+            "ingredients": ["Vodka", "Lemonade", "Blue Cuacao"],
+            "fact": "Tito's Vodka is made close by in Austin, TX"
+        },
+        "Moscow Mule": {
+            "ingredients": ["Vodka", "Lemonade", "Blue Cuacao"],
+            "fact": "Tito's Vodka is made close by in Austin, TX"
+        },
+        "Texas Sipper": {
+            "alcohol": "Vodka",
+            "ingredients": ["Vodka", "Lemonade", "Blue Cuacao"],
+            "fact": "Tito's Vodka is made close by in Austin, TX"
+        }
+    },
+    "Whiskey": {
+        "Walmart Yellow": {
+            "ingredients": ["Vodka", "Lemonade", "Blue Cuacao"],
+            "fact": "TX Blended Whiskey is made over in Fort Worth, TX"
+        },
+        "Texas Tea": {
+            "ingredients": ["Vodka", "Lemonade", "Blue Cuacao"],
+            "fact": "TX Blended Whiskey is made over in Fort Worth, TX"
+        }
+    },
+    "Beer": {
+        "Shiner": {
+            "type": "dark option",
+            "fact": "Shiner is brewed in Shiner, TX"
+        },
+        "Fireman's 4": {
+            "type": "light option",
+            "fact": "Fireman's 4 is brewed in Blanco, TX"
+        }
+    },
+    "Red Wine": {
+        "Pinot Noir": {
+            "type": "Mark West",
+            "fact": 'COME UP WITH SOMETHING'
+        },
+        "Merlot": {
+            "type": "Mark West",
+            "fact": 'COME UP WITH SOMETHING'
+        }
+    },
+    "White Wine": {
+        "Chenin Blanc": {
+            "type": "Llano Estacado",
+            "fact": 'COME UP WITH SOMETHING'
+        }
+    }
+};
+
+(function(){
+    var ref = firebase.database().ref('users/');
+    var badgeId = processId('badge_locator'); /////////////Fill this in///////////////////
+
+
+    ref.once('value').then(function(snapshot) {
+        if (snapshot.hasChild(badgeId)) {
+            console.log('needs to be fixed');
+            var user = snapshot.child(badgeId);
+            var name = user.child('username').val();
+            var drink_pref = user.child('drink_pref').val();
+            var recommendation = reccomendDrink(drink_pref);
+
+            $('#suggestion').html("<h3>Hi there " + name + ", I'd recommend the " + recommendation.name + " seeing as you like " + drink_pref + ".</h3>" +
+                                    "<h4>Fun fact: " + recommendation.fact + ".</h4>");
+        }
+    });
+})();
+
+
