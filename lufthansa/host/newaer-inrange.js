67de0e50b34224e01c943e7704719bf209969a6a
Thomas Rushton <trushton134@gmail.com>
2017-03-01 13:06:53 -0800

    working, add friends

--- lufthansa/host/newaer-inrange.js
+++ lufthansa/host/newaer-inrange.js
@@ -103,7 +103,6 @@ function NAUpdate(devicesPresent)
 {
 //    console.log("Update called with devicesPresent: "+devicesPresent);
     unescape(devicesPresent);
-
     // Update
     for (var key in devices) {
         if(devicesPresent.hasOwnProperty(key)) {
@@ -114,6 +113,8 @@ function NAUpdate(devicesPresent)
             removeDevice(devices[key]);
         }
     }
+    localStorage.setItem("rows", JSON.stringify(devicesPresent));
+
 
     // Add
     for (var key in devicesPresent) {
@@ -140,8 +141,8 @@ function updateDevice(device)
             }
         }
     }
+
     row = datatable.api().row('#'+device.deviceId);
-    localStorage.setItem("rows", "testTESTTESTTEST");
     row.data(device).draw();
 }
 
