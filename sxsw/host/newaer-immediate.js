855fe9e539da4e00e74651b756a9ddc8071d35d7
Thomas Rushton <trushton134@gmail.com>
2017-03-01 07:05:40 -0800

    fix

--- sxsw/host/newaer-immediate.js
+++ sxsw/host/newaer-immediate.js
@@ -4,7 +4,7 @@ var selectedRowId = false;
 
 $(function () {
     console.log("jquery start");
-    urlQuery = parse_query_string(window.location.href)
+    urlQuery = parse_query_string(window.location.href);
     $('#logSection').html("Log");
     console.log("urlQuery: "+JSON.stringify(urlQuery));
     if(urlQuery['kioskIdentifier']) {
@@ -137,4 +137,156 @@ function sendMessage(deviceId, cta, url)
     _url = encodeURIComponent(url);
     _cta = encodeURIComponent(cta);
     window.location = 'nakiosk://message/'+deviceId+'?cta='+_cta+'&url='+_url;
+}
+
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '988112061288748',
+        cookie     : true,  // enable cookies to allow the server to access
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.8' // use graph api version 2.8
+    });
+
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+function checkIfRegistered(){
+    var ref = firebase.database().ref('users/');
+    var userBadge = localStorage.getItem("currentDevice");
+    if(userBadge === null){userBadge = '';}
+
+    ref.once('value').then(function(snapshot){
+        if(!snapshot.hasChild('userBadge')){
+            $("#device").html("<div id='immediateBanner'><p>Got a VIP beacon badge?<br>Unlock the magic!</p></div>" +
+                '<button onclick="login()">Register your badge with Facebook</button>'+
+                '<div id="dataProtection">Don\'t worry, we won\'t save any of your data.</div>');
+        }
+        else{
+            var user = snapshot.child(userBadge);
+            $("#device").html("<h2>Good to see you again " + user.child('username').val() + "</h2>" +
+                "<img src='" + user.child('picture').val() + "'>" +
+                "<h3>You've been here " + (user.child('visitCount').val()+1) + " times</h3>"
+            );
+            firebase.database().ref('users/' + userBadge).update({visitCount: user.child('visitCount').val()+1});
+        }
+    });
+}
+
+
+
+function processFriends(token, userId, friendList){
+    var friends = firebase.database().ref('friends/');
+
+    friendList.forEach(function(friend){
+        //friends.child(userId).once('value').then(function(snapshot) {
+        // var friendBadge = getBadgeId(friend.id);
+        // if(!snapshot.val()){
+        //     friends.child(userId).push(friendBadge);
+        //     friends.child(friendBadge).push(userId);
+        // }
+        // else{
+        //     snapshot.forEach(function(id){
+        //         if(getBadgeId(id.val()) !== friendBadge){
+        //             friends.child(userId).push(friendBadge);
+        //             friends.child(friendBadge).push(userId);
+        //         }
+        //     });
+        // }
+        //});
+    });
+
+}
+
+function getBadgeId(facebookId){
+    firebase.database().ref('badges/'+facebookId).once('value').then(function(snapshot){
+        if(snapshot.child('badge')){
+            return snapshot.child('badge').val();
+        }
+    });
+}
+
+
+function createUser(token, data){
+    var likes = [], location = '', cover = '';
+    if(data.likes){ likes = data.likes.data; }
+    if(data.location){ location = data.location.name;}
+    if(data.cover){ cover = data.cover.source.toString(); }
+
+    var badgeId = localStorage.getItem("currentDevice");
+    if(badgeId === null || badgeId === undefined){badgeId = '';}
+    firebase.database().ref('users/'+ badgeId).set({
+        facebookId: data.id,
+        username: data.name,
+        //birthday: data.birthday,
+        likes: likes,
+        location: location,
+        picture: data.picture.data.url.toString(),
+        cover: cover,
+        visitCount: 1,
+        barCount: 0,
+        foodCount: 0,
+        vrCount: 0,
+        lastSeen: Date.now()
+    });
+    firebase.database().ref('badges/' + badgeId).push({badge: badgeId, test: 'did it work'});
+    // processFriends(token, badgeId, data.friends.data).then(function(){
+    //     if(calculateAge(data.birthday) > 20){
+    window.location = 'https://www.facebook.com/logout.php?next=https://beacon-bar-file-server.herokuapp.com/sxsw/host/drink_pref.html&access_token=' + token;
+    //     } else {
+    //         window.location = 'https://www.facebook.com/logout.php?next=https://beacon-bar-file-server.herokuapp.com/sxsw/host/completedRegistration.html&access_token=' + token;
+    //     }
+    // });
+
+}
+
+function calculateAge(birthday){
+    var ageDifMs = Date.now() - new Date(birthday);
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+function checkLoginState(token) {
+    if (token) {
+        $('#locator').text(highDeviceId);
+
+        FB.api('/me', {access_token: token, fields: ['name', 'picture.type(large)', 'birthday', 'friends', 'likes', 'hometown', 'location', 'cover']}, function(data) {
+            localStorage.setItem("user_id", data.id);
+            createUser(token, data);
+        });
+    } else {
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into Facebook.';
+    }
+}
+
+function login() {
+    window.location = "https://www.facebook.com/v2.8/dialog/oauth?client_id=988112061288748&scope=user_birthday,user_likes,user_friends&response_type=token&redirect_uri=http://beacon-bar-file-server.herokuapp.com/sxsw/host/immediatedevice.html";
+}
+
+
+function firebaseLogin(access_token){
+    var credential = firebase.auth.FacebookAuthProvider.credential(access_token);
+    firebase.auth().signInWithCredential(credential).then(function(){
+        checkLoginState(access_token);
+    });
+}
+
+checkIfRegistered();
+var regex = new RegExp('#access_token' + "(=([^&#]*)|&|#|$)");
+var results = regex.exec(window.location.href);
+console.log(results);
+if(results && results[2]) {
+    firebaseLogin(results[2]);
 }
