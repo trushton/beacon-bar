48bb6a51ed350b72be4179513a2766e74ae2e380
Thomas Rushton <trushton134@gmail.com>
2017-03-01 11:04:24 -0800

    page1

--- sxsw/host/completedRegistration.js
+++ sxsw/host/completedRegistration.js
@@ -1,5 +1,5 @@
 function displayData(){
-    var ref = firebase.database().ref('users/'+localStorage.getItem('user_id'));
+    var ref = firebase.database().ref('users/'+localStorage.getItem('currentDevice'));
     ref.once('value').then(function(snapshot){
         $('.user').text("Thank you " + snapshot.child('username').val()
         + " for registering for the lounge. Redirecting shortly");
