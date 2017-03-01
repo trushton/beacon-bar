484d905b180b6bc3b91d6579734bee82658f90cc
Thomas Rushton <trushton134@gmail.com>
2017-03-01 12:46:47 -0800

    working, add friends

--- lufthansa/host/newaer-inrange.js
+++ lufthansa/host/newaer-inrange.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
         "drawCallback": function ( settings ) {
             var api = this.api();
             var rows = api.rows( {page:'current'} ).nodes();
-            localStorage.setItem("rows", rows.toString());
+            localStorage.setItem("rows", "testTESTTESTTEST");
             var last=null;
 
             api.column(0, {page:'current'} ).data().each( function ( group, i ) {
