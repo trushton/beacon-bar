322930538d5e709858cf81cb91888cf248b711c6
Thomas Rushton <trushton134@gmail.com>
2017-03-02 10:56:15 -0800

    adding style

--- sxsw/host/drink_pref.js
+++ sxsw/host/drink_pref.js
@@ -1,16 +1,8 @@
 function finishRegistration(value){
-    firebase.database().ref('users/'+ localStorage.getItem("currentDevice")).update({
+    firebase.database().ref('users/'+ localStorage.getItem('currentDevice')).update({
         drink_pref: value.toString()
     }).then(function(){
         window.location = "/sxsw/host/completedRegistration.html";
     });
 
 }
-
-//$(document).ready(function () {
-//    firebase.database().ref('users/').once('value', function(snapshot){
-//        if(snapshot.hasChild(badge)){
-//
-//        }
-//    });
-//});
