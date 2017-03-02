2e7060c7e8c42deecf6b1a8ae6ee742d830a0562
Thomas Rushton <trushton134@gmail.com>
2017-03-02 08:35:11 -0800

    add nodevice style

--- sxsw/host/drink_pref.js
+++ sxsw/host/drink_pref.js
@@ -1,5 +1,8 @@
+var badge = localStorage.getItem("currentDevice");
+
+
 function finishRegistration(value){
-    firebase.database().ref('users/'+ localStorage.getItem("currentDevice")).update({
+    firebase.database().ref('users/'+ badge).update({
         drink_pref: value.toString()
     }).then(function(){
         window.location = "/sxsw/host/completedRegistration.html";
@@ -8,5 +11,9 @@ function finishRegistration(value){
 }
 
 (function () {
+    firebase.database().ref('users/').once('value', function(snapshot){
+        if(snapshot.hasChild(badge)){
 
+        }
+    });
 })();
