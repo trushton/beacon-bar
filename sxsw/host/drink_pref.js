1db5429d32c51cb9f8e0c7526f2617894fc9019e
Thomas Rushton <trushton134@gmail.com>
2017-03-02 10:45:12 -0800

    adding style

--- sxsw/host/drink_pref.js
+++ sxsw/host/drink_pref.js
@@ -1,8 +1,5 @@
-var badge = localStorage.getItem("currentDevice");
-
-
 function finishRegistration(value){
-    firebase.database().ref('users/'+ badge).update({
+    firebase.database().ref('users/'+ localStorage.getItem("currentDevice")).update({
         drink_pref: value.toString()
     }).then(function(){
         window.location = "/sxsw/host/completedRegistration.html";
