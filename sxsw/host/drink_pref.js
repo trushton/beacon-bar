40380091bbfbdf8c3de93a9dbfff5781e8c67c8a
Thomas Rushton <trushton134@gmail.com>
2017-03-03 08:10:12 -0800

    styling

--- sxsw/host/drink_pref.js
+++ sxsw/host/drink_pref.js
@@ -9,18 +9,14 @@ function finishRegistration(value){
 }
 
 $('document').ready(function(){
-    console.log(localStorage.getItem('user_id'));
-    console.log(localStorage.getItem('currentDevice'));
-    firebase.database().ref('badges/'+localStorage.getItem('user_id')).once('value').then(function(snapshot){
-        badge = snapshot.child('badge').val();
-        firebase.database().ref('users/'+badge).once('value').then(function(snapshot){
-            if(snapshot.hasChild('picture')){
-                var image = document.createElement(img);
-                var parent = document.getElementById('bod');
-                image.id = 'userImage';
-                image.src = snapshot.child('picture').val();
-                parent.appendChild(image);
-            }
-        });
+    badge = localStorage.getItem('currentDevice');
+    firebase.database().ref('users/'+badge).once('value').then(function(snapshot){
+        if(snapshot.hasChild('picture')){
+            var image = document.createElement(img);
+            var parent = document.getElementById('bod');
+            image.id = 'userImage';
+            image.src = snapshot.child('picture').val();
+            parent.appendChild(image);
+        }
     });
 });
