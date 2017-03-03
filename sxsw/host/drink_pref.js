b86a3a2d49518a5f546790a017cff90ee83edc91
Thomas Rushton <trushton134@gmail.com>
2017-03-03 08:02:15 -0800

    styling

--- sxsw/host/drink_pref.js
+++ sxsw/host/drink_pref.js
@@ -9,6 +9,8 @@ function finishRegistration(value){
 }
 
 $('document').ready(function(){
+    console.log(localStorage.getItem('user_id'));
+    console.log(localStorage.getItem('currentDevice'));
     firebase.database().ref('badges/'+localStorage.getItem('user_id')).once('value').then(function(snapshot){
         badge = snapshot.child('badge').val();
         firebase.database().ref('users/'+badge).once('value').then(function(snapshot){
