1547893aad51ade4199f6212e18164fa72ccfb0d
Thomas Rushton <trushton134@gmail.com>
2017-03-05 13:25:30 -0800

    inrangedevices working

--- VRKiosk/host/newaer-immediatedevices.js
+++ VRKiosk/host/newaer-immediatedevices.js
@@ -1,6 +1,4 @@
-var datatable;
 var devices = null;
-var deviceArray = [];
 var selectedRowId = false;
 
 
@@ -14,7 +12,11 @@ $(function () {
     }
 });
 
+$(document).ready(function() {
+    console.log("document ready");
+});
 
+var groups = [ 'Immediate','Near','Far', 'Unknown'];
 
 function parse_query_string(string)
 {
@@ -35,6 +37,9 @@ function parse_query_string(string)
     b['page'] = page;
     return b;
 }
+
+var highRssi;
+var highDeviceId;
 
 function NAUpdate(devicesPresent)
 {
@@ -71,6 +76,7 @@ function NAUpdate(devicesPresent)
     if(highDeviceId != "") {
         localStorage.setItem("currentDevice", devices[highDeviceId].data.recordLocator);
     }
+    updateTimers();
 }
 
 function updateDevice(device) {
@@ -81,23 +87,22 @@ function updateDevice(device) {
 
     deviceDbRecord.once('value').then(function(currentRecord){
         firebase.database().ref('vrQueue/').once('value').then(function(vrQueue){
-           if(!vrQueue.hasChild('badge')) {
-               if(device.rssi > -70){
-                   deviceDbRecord.update({ vrEnqueueTimer: (currentRecord.child('vrEnqueueTimer').val() + 1) });
-                   if(currentRecord.child('vrEnqueueTimer').val() > 30){
-                       firebase.database().ref('vrQueue/' + badge).update({
-                           timeEntered: Date.now()
-                       });
-                       deviceDbRecord.update({ vrEnqueueTimer: 0 });
-                   }
-               }
-               else{
-                   deviceDbRecord.update({ vrEnqueueTimer: 0 });
-               }
-           }
+            if(!vrQueue.hasChild('badge')) {
+                if(device.rssi > -70){
+                    deviceDbRecord.update({ vrEnqueueTimer: (currentRecord.child('vrEnqueueTimer').val() + 1) });
+                    if(currentRecord.child('vrEnqueueTimer').val() > 30){
+                        firebase.database().ref('vrQueue/' + badge).update({
+                            timeEntered: Date.now()
+                        });
+                        deviceDbRecord.update({ vrEnqueueTimer: 0 });
+                    }
+                }
+                else{
+                    deviceDbRecord.update({ vrEnqueueTimer: 0 });
+                }
+            }
         });
     })
-
 }
 
 
@@ -117,14 +122,11 @@ function parseId(data){
 
 function removeDevice(device)
 {
-//    console.log("Removing device: "+device.deviceId);
     delete devices[device.deviceId];
-    datatable.api().row('#'+device.deviceId).remove().draw();
 }
 
 function addDevice(device)
 {
-//    console.log("Adding device: "+device.deviceId);
     if(devices == null) devices = Object;
     if(typeof device.data === 'undefined' || typeof device.data.name === 'undefined') {
         device.data.name = device.name;
@@ -140,18 +142,41 @@ function addDevice(device)
             }
         }
     }
-
     devices[device.deviceId] = device;
-    rowNode = datatable.api().row.add(device).draw().node();
-    $(rowNode).attr('id',device.deviceId);
 }
 
-function sendMessage(deviceId, cta, url)
-{
-    console.log("Sending message to "+deviceId);
-    console.log(" with cta: "+cta);
-    console.log(" and url: "+url);
-    _url = encodeURIComponent(url);
-    _cta = encodeURIComponent(cta);
-    window.location = 'nakiosk://message/'+deviceId+'?cta='+_cta+'&url='+_url;
+function updateTimers(){
+    var database = firebase.database();
+    var guestData = [];
+
+    database.ref('vrQueue').orderByChild('timeEntered').limitToFirst(3).once('value').then(function(currentQueue){
+        database.ref('users/').once('value').then(function(queuedGuests){
+            currentQueue.forEach(function(guest){
+                guestData.push({waitTime: getSecondsSince(currentQueue.child(guest.key).child('timeEntered').val()),
+                    name: queuedGuests.child(guest.key).child('username').val(),
+                    picture: queuedGuests.child(guest.key).child('picture').val()});
+            });
+        }).then(function() {
+            var queueSource = $('#queued-guests-template').html();
+            var queueTemplate = Handlebars.compile(queueSource);
+
+            var queueHtml = queueTemplate({
+                guest1: checkIfGuest(guestData,0),
+                guest2: checkIfGuest(guestData, 1),
+                guest3: checkIfGuest(guestData, 2)
+            });
+
+            $('[data-queue-next-three]').html(queueHtml);
+        })
+    });
+}
+
+function checkIfGuest(data, index){
+    if(data[index]){return data[index];}
+}
+
+
+function getSecondsSince(time) {
+    var timeDiff = Math.floor((Date.now() - time) / 1000);
+    return Math.floor((timeDiff / 60)) + ":" + (timeDiff % 60);
 }
